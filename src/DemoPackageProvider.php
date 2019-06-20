79c9f5e86763105fa5046a5cbab19eb4337e1221
longnd <longnd@viralsoft.vn>
2019-06-20 22:14:57 +0700

    first commit

---
+++ src/DemoPackageProvider.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Longnd\DemoPackage;
+
+use Illuminate\Support\ServiceProvider;
+
+class DemoPackageProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__.'/config/demopackage.php', 'demopackage'
+        );
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/config/demopackage.php' => config_path('demopackage.php'),
+        ]);
+
+        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
+
+        $this->loadTranslationsFrom(__DIR__.'/resources/lang/en', 'demo');
+
+        $this->publishes([
+            __DIR__.'/resources/lang' => resource_path('lang/vendor/demo'),
+        ]);
+
+        $this->loadViewsFrom(__DIR__.'/resources/views', 'demopackage');
+
+        $this->loadRoutesFrom(__DIR__.'/routes/demo.php');
+    }
+}
---
+++ src/resources/lang/en/demo.php
@@ -0,0 +1,5 @@
+<?php
+return [
+    'demo' => 'Demo Package Development',
+    'hello' => 'Demo Package Development',
+];
